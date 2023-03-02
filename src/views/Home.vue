3410dd4142ed75494bd299dabcfd67434ab7c32f
beryl2 <berylakinyi150@gmail.com>
2023-03-02 16:54:50 +0300

    alphabet ans the search button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,16 @@
 <template>
-    <pre>{{  meals }}</pre> 
+    <div class="flex flex-col p-8 items-center justify-center">
+        <div>
+            <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search the meal"/>
+        </div>
+        
 
+        <div class="flex gap-1">
+            <route-link to="/" v-for="letter of letters">
+                {{ letters }}
+            </route-link>
+        </div>
+    </div>
 
 </template>
 
@@ -8,6 +18,7 @@
 import { computed } from 'vue';
 import store from '../store';
 
-const meals= computed(() => Store.state.meals)
+const meals= computed(() => store.state.meals);
+const  letters= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 </script>
 
