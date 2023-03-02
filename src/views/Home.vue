d5c9f3a8e349bcd399a1386927628941735ed62d
beryl2 <berylakinyi150@gmail.com>
2023-03-02 14:37:44 +0300

    Initalization of Mealapp

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <pre>{{  meals }}</pre> 
+
+
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import store from '../store';
+
+const meals= computed(() => Store.state.meals)
+</script>
+
